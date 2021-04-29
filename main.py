b1e8f654f55cbfbaef937ddbbf8104683c87b1d9
rabeeh-ta <rabeehchemnad@gmail.com>
2021-04-29 18:14:52 +0530

    draw the char

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 
-col1 = ["" for i in range(1, 53)]
-col2 = ["" for i in range(1, 53)]
-col3 = ["" for i in range(1, 53)]
-col4 = ["" for i in range(1, 53)]
-col5 = ["" for i in range(1, 53)]
-col6 = ["" for i in range(1, 53)]
-col7 = ["" for i in range(1, 53)]
+col = 10
+
+col1 = [" " for i in range(1, col)]
+col2 = [" " for i in range(1, col)]
+col3 = [" " for i in range(1, col)]
+col4 = [" " for i in range(1, col)]
+col5 = [" " for i in range(1, col)]
+col6 = [" " for i in range(1, col)]
+col7 = [" " for i in range(1, col)]
 
 mat = [col1, col2, col3, col4, col5, col6, col7]
 
@@ -15,11 +17,17 @@ def print_mat():
         print(row)
 
 
-def draw_mat(x, y):
-    print(f"row: {x} , column: {y}")
-    mat[x][y] = "*"
+def draw_mat(ch):
+    for (x, y) in ch:
+        print(x, y)
 
-    print_mat()
+    # print_mat()
 
 
-draw_mat(0, 0)
+def name_to_mat(ltr):
+    if('A' == ltr):
+        return
+
+
+draw_mat((((0, 0), (1, 0), (2, 0), (3, 0), (4, 0)), ((0, 1), (), (2, 1), (), ()),
+          ((0, 2), (), (2, 2), (), ()), ((0, 3), (1, 3), (2, 3), (3, 3), (4, 3))))
