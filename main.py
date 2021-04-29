293f5a5e8251573e53b19d4323160d2a9d7a1ecc
rabeeh-ta <rabeehchemnad@gmail.com>
2021-04-29 23:33:41 +0530

    Refact

--- main.py
+++ main.py
@@ -18,30 +18,36 @@ def print_mat():
     for row in mat:
         print(row)
 
+# to print the matrix with out the arrays
+
 
 def print_plain_mat():
     for row in mat:
-        for element in row:
-            print(element)
-        print('/n')
+        sttr = ''
+        for elemnt in row:
+            sttr = sttr + elemnt
+        print(sttr)
+
 
 # for drawing the individual co-ordinates.
-
-
 def draw_mat(ch):
     for x, y in ch:
-        mat[x][y] = '@'
+        mat[x+1][y+1] = '*'
     print_plain_mat()
+    print_mat()
+
 
 # change the given charector to the co-ord in matrix
-# a charector is represented useing 5 rows and 4 columns.
-
-
+# a charector is represented using 5 rows and 4 columns.
 def name_to_mat(ltr):
+    ltr = ltr.upper()
     if('A' == ltr):
         return ((0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (0, 1),  (2, 1),
                 (0, 2), (2, 2), (0, 3), (1, 3), (2, 3), (3, 3), (4, 3))  # column wise is done here
+    elif('B' == ltr):
+        return ()
 
 
-draw_mat(((0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (0, 1),  (2, 1),
-          (0, 2), (2, 2), (0, 3), (1, 3), (2, 3), (3, 3), (4, 3)))
+matrix_code = name_to_mat("a")
+
+draw_mat(matrix_code)
