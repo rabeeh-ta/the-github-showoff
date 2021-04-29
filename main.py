7e0d89a7d90ded32304b8945056e9f27392b3b72
Rabeeh T A <43750842+rabeeh-ta@users.noreply.github.com>
2021-04-29 19:39:28 +0530

    added char to matrix convert function

--- main.py
+++ main.py
@@ -11,23 +11,37 @@ col7 = [" " for i in range(1, col)]
 
 mat = [col1, col2, col3, col4, col5, col6, col7]
 
+# will print the matrix
+
 
 def print_mat():
     for row in mat:
         print(row)
 
 
-def draw_mat(ch):
-    for (x, y) in ch:
-        print(x, y)
+def print_plain_mat():
+    for row in mat:
+        for element in row:
+            print(element)
+        print('/n')
 
-    # print_mat()
+# for drawing the individual co-ordinates.
+
+
+def draw_mat(ch):
+    for x, y in ch:
+        mat[x][y] = '@'
+    print_plain_mat()
+
+# change the given charector to the co-ord in matrix
+# a charector is represented useing 5 rows and 4 columns.
 
 
 def name_to_mat(ltr):
     if('A' == ltr):
-        return
+        return ((0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (0, 1),  (2, 1),
+                (0, 2), (2, 2), (0, 3), (1, 3), (2, 3), (3, 3), (4, 3))  # column wise is done here
 
 
-draw_mat((((0, 0), (1, 0), (2, 0), (3, 0), (4, 0)), ((0, 1), (), (2, 1), (), ()),
-          ((0, 2), (), (2, 2), (), ()), ((0, 3), (1, 3), (2, 3), (3, 3), (4, 3))))
+draw_mat(((0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (0, 1),  (2, 1),
+          (0, 2), (2, 2), (0, 3), (1, 3), (2, 3), (3, 3), (4, 3)))
