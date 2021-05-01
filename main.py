b36b7011be519753241a568f90c0edfce7941314
rabeeh-ta <rabeehchemnad@gmail.com>
2021-05-01 16:17:26 +0530

    print all letter func

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 """
-    TODO:
+    !ROADMAP!
      complete the alphabets (refact to new file).
      new func for printing at most 10 letters in the matrix.
      algorithm to generate the dates from the matrix.
-     get the git commit library or make one. 
+     get the git commit library or make one.
      generate standalone folder with all the git stuff ready to push.
 """
-col = 10
+col = 52
 
 col2 = [" " for i in range(0, col)]
 col1 = [" " for i in range(0, col)]
@@ -19,20 +19,20 @@ col7 = [" " for i in range(0, col)]
 mat = [col1, col2, col3, col4, col5, col6, col7]
 
 
-# will print the matrix
+# ? will print the matrix
 def display_mat():
     for row in mat:
         print(row)
 
 
-# for clearing the display while printing many charector one by one
+# ? for clearing the display while printing many charector one by one
 def clear_display():
     for i in range(0, 7):
         for j in range(0, col):
             mat[i][j] = " "
 
 
-# to print the matrix with out the arrays
+# ? to print the matrix with out the arrays
 def display():
     for row in mat:
         sttr = ""
@@ -41,18 +41,17 @@ def display():
         print(sttr)
 
 
-# for drawing the individual co-ordinates.
-def draw_mat(ch):
+# ? for drawing the individual co-ordinates.
+def draw_mat(ch, pos=1):  # pos for which co-ord to start the letter
     # clear the display for each latter print > this is only for testing the letter
-    clear_display()
+    # clear_display()
     for x, y in ch:
-        mat[x + 1][y + 1] = "*"
-    display()  # display the matrix without arrays and quotes
-    # display_mat() # display the arrays which is the matrix
+        mat[x + 1][pos + y] = "*"  # pos will leave the required space
+    # display()  # display the matrix without arrays and quotes
+    # display_mat()  # display the arrays which is the matrix
 
 
-# change the given charector to the co-ord in matrix
-# a charector is represented using 5 rows and 4 columns.
+# ? change the given charector to the co-ord in matrix. a charector is represented using 5 rows and 4 columns.
 def char_to_coords(ltr):
     ltr = ltr.upper()
     if('A' == ltr):
@@ -75,7 +74,7 @@ def char_to_coords(ltr):
                 (0, 2), (0, 3), (2, 1), (2, 2), (2, 3))
     elif('G' == ltr):
         return ((0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (0, 1),
-                (0, 2), (0, 3), (2, 2), (2, 3), (4, 1), (4, 2), (4, 3), (3,3))
+                (0, 2), (0, 3), (2, 2), (2, 3), (4, 1), (4, 2), (4, 3), (3, 3))
     elif('H' == ltr):
         return ((0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (0, 3), (2, 2),
                 (2, 3), (4, 3), (2, 1), (3, 3), (1, 3))
@@ -106,11 +105,15 @@ def char_to_coords(ltr):
 
 
 string = "abcdefghijklmnop"
+name = "DOG"
 
-# loop though the string and print every letter
-#for l in string:
-    #char_code = char_to_coords(l)
-    #draw_mat(char_code)
 
-char_code = char_to_coords("g")
-draw_mat(char_code)
+# ? loop though the string and print every letter in diff matrix
+for l in string:
+    clear_display()
+    char_code = char_to_coords(l)
+    draw_mat(char_code)
+    display()
+
+# char_code = char_to_coords("m")
+# draw_mat(char_code)
