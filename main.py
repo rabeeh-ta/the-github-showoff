ecd9173236dc8bd77f6011e93feaa4c98401cf38
rabeeh-ta <rabeehchemnad@gmail.com>
2021-05-02 20:35:46 +0530

    individual dates extraction working

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 from modules.char_to_coords import char_to_coords
 
 
@@ -5,43 +6,57 @@ from modules.char_to_coords import char_to_coords
         !ROADMAP!
     [*] complete the alphabets (refact to new file).
     [*] new func for printing at most 10 letters in the matrix.
-    [ ] algorithm to generate the dates from the matrix.
+    [*] algorithm to generate the dates from the matrix.
     [ ] get the git commit library or make one.
     [ ] generate standalone folder with all the git stuff ready to push.
 """
 col = 52
 
-col2 = [" " for i in range(0, col)]
-col1 = [" " for i in range(0, col)]
-col3 = [" " for i in range(0, col)]
-col4 = [" " for i in range(0, col)]
-col5 = [" " for i in range(0, col)]
-col6 = [" " for i in range(0, col)]
-col7 = [" " for i in range(0, col)]
+#! this looks ugly should do some thing or take it to other file
+matCol1 = [" " for i in range(0, col)]
+matCol2 = [" " for i in range(0, col)]
+matCol3 = [" " for i in range(0, col)]
+matCol4 = [" " for i in range(0, col)]
+matCol5 = [" " for i in range(0, col)]
+matCol6 = [" " for i in range(0, col)]
+matCol7 = [" " for i in range(0, col)]
 
-mat = [col1, col2, col3, col4, col5, col6, col7]
+mat_Date_Col1 = [" " for i in range(0, col)]
+mat_Date_Col2 = [" " for i in range(0, col)]
+mat_Date_Col3 = [" " for i in range(0, col)]
+mat_Date_Col4 = [" " for i in range(0, col)]
+mat_Date_Col5 = [" " for i in range(0, col)]
+mat_Date_Col6 = [" " for i in range(0, col)]
+mat_Date_Col7 = [" " for i in range(0, col)]
 
-
-# ? will print the matrix
-def display_mat():
-    for row in mat:
-        print(row)
-
-
-# ? for clearing the display while printing many charector one by one
-def clear_display():
-    for i in range(0, 7):
-        for j in range(0, col):
-            mat[i][j] = " "
+mat = [matCol1, matCol2, matCol3, matCol4, matCol5,
+       matCol6, matCol7]  # mat for drawing the stars
+matDates = [mat_Date_Col1, mat_Date_Col2, mat_Date_Col3,
+            mat_Date_Col4, mat_Date_Col5, mat_Date_Col6, mat_Date_Col7]  # a reference calender
+commitDates = []  # the final dates to do the commits
 
 
 # ? to print the matrix with out the arrays
-def display():
+def display(mat):
     for row in mat:
         sttr = ""
         for elemnt in row:
             sttr = sttr + elemnt
         print(sttr)
+
+# ? will print the matrix
+
+
+def display_mat(mat):
+    for row in mat:
+        print(row)
+
+
+# ? for clearing the display while printing many charector one by one
+def clear_display(mat):
+    for i in range(0, 7):
+        for j in range(0, col):
+            mat[i][j] = " "
 
 
 # ? for drawing the individual co-ordinates.
@@ -50,20 +65,30 @@ def draw_mat(ch, pos=1):  # if no poss passed then start from 1st co ord
         # pos will leave the required space so that letters will not get printed on top of each other
         mat[x + 1][pos + y] = "*"
 
-# ? function will return dates in an array
 
-
-def get_dates():
-    commits = 0
+# ? function takes a matrix and year => generates all the dates from JAN to DEC and returns the array
+def mat_dates_gen(year, matDates):
+    commitDate = date(year, 1, 1)
     for y in range(0, 52):
         for x in range(0, 7):
-            if mat[x][y] == "*":
-                commits = commits + 1
-    print(commits)
+            matDates[x][y] = commitDate
+            commitDate += timedelta(days=1)
+    return matDates
+
+
+# ? Func loops through the array and find for * if found then will take the co-ord and will get the corresponding date from the OTHER matrix
+def get_dates(mat, matDates, commitDates):
+    matDates = mat_dates_gen(2020, matDates)  # making the OTHER matrix
+    for y in range(0, 52):
+        for x in range(0, 7):
+            if mat[x][y] == "*":  # check
+                # get the corresponding date
+                commitDates.append(matDates[x][y])
+    return commitDates
 
 
 string = "abcdefghijklmnop"
-name = "DOGE"
+name = "doge"
 
 # ? PRINT => all char in one matrix
 if len(name) <= 10:
@@ -72,23 +97,26 @@ if len(name) <= 10:
         pos = (indx * 5)
         char_code = char_to_coords(l)
         draw_mat(char_code, pos)
-    display()
+    display(mat)
 else:
     print("string should be 10 or less")
 
 
 # ? PRINT => all char diff matrix
 # for l in string:
-#     clear_display()
+#     clear_display(mat)
 #     char_code = char_to_coords(l)
 #     draw_mat(char_code)
-#     display()
+#     display(mat)
 
 # ? PRINT => one letter in one matrix
-#char_code = char_to_coords("z")
+# char_code = char_to_coords("z")
 # draw_mat(char_code)
-# display()
+# display(mat)
 
 
-# ? get the dates fucntion
-get_dates()
+# ? puting everything together
+# commitDates array have the individual dates from all the * spots
+commitDates = get_dates(mat, matDates, commitDates)
+# there are 50 stars in DOGE this array has 50 date-stamps powli man powli
+print(len(commitDates))
